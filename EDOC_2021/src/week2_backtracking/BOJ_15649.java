28f448ecba2763bd747ded9848b3ceb63bb6406d
EmilyCh0 <67777523+EmilyCh0@users.noreply.github.com>
2021-01-16 12:48:36 +0900

    N과M bufferedReader,Writer로 변경

--- EDOC_2021/src/week2_backtracking/BOJ_15649.java
+++ EDOC_2021/src/week2_backtracking/BOJ_15649.java
@@ -1,17 +1,19 @@
 package week2_backtracking;
 
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
 public class BOJ_15649 {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static int[] arr = new int[10];
 	static boolean[] isUsed;
 	static int N;
 	static int M;
-	static void backtrack(int k) {
+	static void backtrack(int k) throws IOException {
 		if(k==M) {
 			for(int i=0;i<M;i++) {
-				System.out.print(arr[i]+" ");
+				bw.write(arr[i]+" ");
 			}
-			System.out.println("");
+			bw.newLine();
 		}
 		else {
 			for(int j=0;j<N;j++) {
@@ -21,14 +23,18 @@ public class BOJ_15649 {
 					backtrack(k+1);
 					isUsed[j]=false;
 				}
+				
 			}
 		}
 	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		N = scan.nextInt();
-		M = scan.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		isUsed = new boolean[N];
 		backtrack(0);
+		bw.flush();
 	}
 }
+
--- EDOC_2021/src/week2_backtracking/BOJ_15650.java
+++ EDOC_2021/src/week2_backtracking/BOJ_15650.java
@@ -1,17 +1,19 @@
 package week2_backtracking;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 public class BOJ_15650 {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static int[] arr = new int[10];
 	static boolean[] isUsed;
 	static int N;
 	static int M;
-	static void backtrack(int k, int idx) {
+	static void backtrack(int k, int idx) throws IOException {
 		if(k==M) {
 			for(int i=0;i<M;i++) {
-				System.out.print(arr[i]+" ");
+				bw.write(arr[i]+" ");
 			}
-			System.out.println("");
+			bw.newLine();
 		}
 		else {
 			for(int j=idx;j<N;j++) {
@@ -21,14 +23,56 @@ public class BOJ_15650 {
 					backtrack(k+1,j);
 					isUsed[j]=false;
 				}
+				
 			}
 		}
 	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		N = scan.nextInt();
-		M = scan.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		isUsed = new boolean[N];
 		backtrack(0,0);
+		bw.flush();
 	}
 }
+/*
+import java.io.*;
+import java.util.StringTokenizer;
+public class BOJ_15650 {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int[] arr = new int[10];
+	static boolean[] isUsed;
+	static int N;
+	static int M;
+	static void backtrack(int k, int idx) throws IOException {
+		if(k==M) {
+			for(int i=0;i<M;i++) {
+				bw.write(arr[i]+" ");
+			}
+			bw.newLine();
+		}
+		else {
+			for(int j=0;j<N;j++) {
+				if(!isUsed[j]) {
+					arr[k]=j+1;
+					isUsed[j]=true;
+					backtrack(k+1,j);
+					isUsed[j]=false;
+				}
+				
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		isUsed = new boolean[N];
+		backtrack(0,0);
+		bw.flush();
+	}
+}
+*/
